be38dfdb8185ee91aef74fcba0deb28630583ed3
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-08 21:35:08 -0400

    changing list font size' '

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Ugly Query
 
 This project will be JUST like [Ugly Webby](https://github.com/ci-wdi-900/ugly-webby), but this time you'll be editing the JavaScript file ONLY. (Except if, for convenience, you want to add IDs or classes to elements in `index.html`.) Run queries with `querySelector` and `createElement`s until you have the ugliest page possible!
+
+can div2.querySelector
+
+const thatDiv = document.querySelector('#that-div')
---
+++ main.js
@@ -0,0 +1,38 @@
+const headline = document.querySelector('h1');
+
+headline.style.font = 'italic bold  comic sans ms,serif';
+headline.style.color = 'brown';
+headline.style.textShadow = '22px 2px 5px green';
+headline.style.letterSpacing= '-50px';
+headline.style.scrol
+
+const paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue'
+paragraph.style.fontSize = '50px'
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+
+const body = document.querySelector('body');
+body.style.backgroundImage = "url('http://www.tioxic.com/wp-content/uploads/trex_4.jpg')";
+
+const item1 = document.querySelectorAll('#item-1');
+item1[0].style.color = "red"
+item1[1].style.color = "red"
+item1[0].style.fontsize = "60px"
+item1[1].style.fontsize = "30px"
+
+const item2 = document.querySelectorAll('#item-2');
+item2[0].style.color = "green"
+item2[1].style.color = "green"
+const item3 = document.querySelectorAll('#item-3');
+item3[0].style.color = "yellow"
+item3[1].style.color = "yellow"
+const item4 = document.querySelectorAll('#item-4');
+item4[0].style.color = "aqua"
+item4[1].style.color = "aqua"
+const item5 = document.querySelectorAll('#item-5');
+item5[0].style.color = "maroon"
+
+
+const form = document.querySelector('textarea');
+form.stye.b = 'blue'
